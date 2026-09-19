776fb34b6b26c66be15904f0b074aaca417e8804
Juan Alvarez Ruiz <juandiegoalvarezruiz.083@gmail.com>
2026-09-18 22:08:26 -0300

    Annotations en clases Articulo, Marca y Categoria

--- TPWinForm/Dominio/Articulo.cs
+++ TPWinForm/Dominio/Articulo.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Dominio
 {
     public class Articulo
     {
         public int Id { get; set; }
+        [DisplayName("Código")]
         public string Codigo { get; set; }
         public string Nombre { get; set; }
+        [DisplayName("Descripción")]
         public string Descripcion { get; set; }
         public int IdMarca { get; set; }
         public int IdCategoria { get; set; }
         public Marca Marca { get; set; }
+        [DisplayName("Categoría")]
         public Categoria Categoria { get; set; }
         public decimal Precio { get; set; }
     } 
--- TPWinForm/Dominio/Categoria.cs
+++ TPWinForm/Dominio/Categoria.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Dominio
 {
     public class Categoria
     {
         public int Id { get; set; }
+        [DisplayName("Descripción")]
         public string Descripcion { get; set; }
 
         public override string ToString()
